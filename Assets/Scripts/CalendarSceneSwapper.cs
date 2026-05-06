c30d6dbfbb422fa5f47bb0369b3fa562ce2fae25
LukeR4704 <C23347841@mytudublin.ie>
2026-05-06 18:48:50 +0100

    Added calendar change script w/ sound effect

--- Assets/Scripts/CalendarSceneSwapper.cs
+++ Assets/Scripts/CalendarSceneSwapper.cs
@@ -1,20 +1,57 @@
+using System.Collections;
 using UnityEngine;
+
 public class CalendarSceneSwapper : MonoBehaviour
 {
-    //just a temp script so i can swap days when testing in a method similar to the intended way
-    float timer;
+    [Header("Calendar Days In Order")]
+    public GameObject[] days;
+
+    [Header("Audio")]
+    public AudioSource markerAudio;
+
     private void OnEnable()
     {
-        timer = 0;
+        StartCoroutine(CalendarSequence());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator CalendarSequence()
     {
-        timer += Time.deltaTime * 4;
-        if(timer > 2)
+        int newDay = TimeManagerScript.Instance.currentDay;
+        int previousDay = newDay - 1;
+
+        // Turn all days off
+        foreach (GameObject day in days)
         {
-            TimeManagerScript.Instance.CalendarToRooms();
+            day.SetActive(false);
         }
+
+        // Show previous day
+        if (previousDay >= 0 && previousDay < days.Length)
+        {
+            days[previousDay].SetActive(true);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        // Hide previous day
+        if (previousDay >= 0 && previousDay < days.Length)
+        {
+            days[previousDay].SetActive(false);
+        }
+
+        if (markerAudio != null)
+        {
+            markerAudio.Play();
+        }
+
+        // Show new day
+        if (newDay >= 0 && newDay < days.Length)
+        {
+            days[newDay].SetActive(true);
+        }
+
+        yield return new WaitForSeconds(1.5f);
+
+        TimeManagerScript.Instance.CalendarToRooms();
     }
 }
